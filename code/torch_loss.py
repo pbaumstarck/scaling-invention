73817d788e1fbaf61b180b7748b59ab3c61c28c0
pbaumstarck <pbaumstarck@gmail.com>
2024-02-17 09:25:19 -0800

    Updating Torch loss to Medium draft.

--- code/torch_loss.py
+++ code/torch_loss.py
@@ -1,165 +1,194 @@
 
-import pylab
-import torch
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create a loss function
-
-# Create some data
-# x = torch.randn(100, 1)
-# y = torch.randn(100, 1)
-
+np.random.seed(20240215)
 
 n = 50
 x = np.array(np.random.randn(n), dtype=np.float32)
-# x = 3 * np.random.random(n) - 1
-# y = x * x + 2 * x + 1 + 0.1
-y = x * x + 1.0 * x + 2.0 # + 0.1 * np.random.random(n, dtype=np.float32)
-# y = -4.0 * x + 7.0 # + 0.1 * np.random.random(n, dtype=np.float32)
-y += 0.3 * np.random.randn(n)
+y = np.array(0.75 * x**2 + 1.0 * x + 2.0 + 0.3 * np.random.randn(n), dtype=np.float32)
 
-if False:
-  plt.scatter(x, y, facecolors='none', edgecolors='b')
-  plt.scatter(x, y, c='r')
-  plt.show()
+plt.scatter(x, y, facecolors='none', edgecolors='b')
+plt.scatter(x, y, c='r')
+print('Figure 1. Toy problem set of points.')
+plt.show()
 
 
-# # Calculate the loss
-# loss = loss_fn(x, y)
+# ===
 
-# # Plot the loss
-# plt.plot(x, loss)
-# plt.show()
+import torch
 
-# board_array = np.array(board, dtype=np.float32)
-# fig, ax = plt.subplots()
-# i = ax.imshow(board_array, cmap=cm.jet, interpolation='nearest')
-# fig.colorbar(i)
-# plt.show()
-
-# Calculate the loss in parameter space ...
-
+model = torch.nn.Linear(1, 1)
+model.weight.data.fill_(6.0)
+model.bias.data.fill_(-3.0)
 
 loss_fn = torch.nn.MSELoss()
-loss_fn = torch.nn.L1Loss()
-loss_fn = torch.nn.HuberLoss()
-loss_fn = torch.nn.SmoothL1Loss()
+learning_rate = 0.1
+epochs = 100
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-# LOSS_MAP_ORIGIN = -5
-# LASS_MAP_SCALE = 18
+for epoch in range(epochs):
+  inputs = torch.from_numpy(x).requires_grad_().reshape(-1, 1)
+  labels = torch.from_numpy(y).reshape(-1, 1)
 
-# weight and bias
+  optimizer.zero_grad()
+  outputs = model(inputs)
+  loss = loss_fn(outputs, labels)
+  loss.backward()
+  optimizer.step()
+  print('epoch {}, loss {}'.format(epoch, loss.item()))
+
+
+weight = model.weight.item()
+bias = model.bias.item()
+plt.scatter(x, y, facecolors='none', edgecolors='b')
+plt.plot(
+  [x.min(), x.max()],
+  [weight * x.min() + bias, weight * x.max() + bias],
+  c='r')
+print('Figure 2. L2-learned linear boundary on toy problem.')
+plt.show()
+
+
+# ===
+
 def get_loss_map(loss_fn, x, y):
-  losses = [[0.0] * 100 for _ in range(100)]
+  """Maps the loss function on a 100-by-100 grid between (-5, -5) and (13, 13)."""
+  losses = [[0.0] * 101 for _ in range(101)]
   x = torch.from_numpy(x)
   y = torch.from_numpy(y)
-  for wi in range(100):
-    for wb in range(100):
-      w = -5.0 + 13.0 * wi / 100.0
-      b = -5.0 + 13.0 * wb / 100.0
+  for iw in range(101):
+    for ib in range(101):
+      w = -5.0 + 13.0 * iw / 100.0
+      b = -5.0 + 13.0 * ib / 100.0
+
       ywb = x * w + b
+      losses[iw][ib] = loss_fn(ywb, y).item()
 
-      # breakpoint()
-      # x = bias, y = weight
-      # breakpoint()
-      losses[wi][wb] = loss_fn(ywb, y).item()
+  return list(reversed(losses))
 
-  # import pylab
-  # cm = pylab.get_cmap('RdYlGn')
-  # fig, ax = plt.subplots()
-  # i = ax.imshow(losses, cmap=cm, interpolation='nearest')
-  # fig.colorbar(i)
-  # plt.show()
-  # breakpoint()
-  return losses
-  # return list(reversed(losses))
-  # breakpoint()
+loss_fn = torch.nn.MSELoss()
+losses = get_loss_map(loss_fn, x, y)
+
+import pylab
+
+cm = pylab.get_cmap('terrain')
+fig, ax = plt.subplots()
+plt.xlabel('Bias')
+plt.ylabel('Weight')
+i = ax.imshow(losses, cmap=cm, interpolation='nearest', extent=[-5, 8, -5, 8])
+fig.colorbar(i)
+print('Figure 3. L2 loss function on toy problem.')
+plt.show()
 
 
-# class linearRegression(torch.nn.Module):
-#   def __init__(self, inputSize, outputSize):
-#     super(linearRegression, self).__init__()
-#     self.linear = torch.nn.Linear(inputSize, outputSize)
+# ===
 
-#   def forward(self, x):
-#     return self.linear(x)
+model = torch.nn.Linear(1, 1)
+model.weight.data.fill_(6.0)
+model.bias.data.fill_(-3.0)
 
-# LEARN!
-def learn(criterion, x, y, lr=0.1, epochs=100):
+loss_fn = torch.nn.MSELoss()
+learning_rate = 0.1
+epochs = 100
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0 * 0.9)
+
+models = [[model.weight.item(), model.bias.item()]]
+for epoch in range(epochs):
+  inputs = torch.from_numpy(x).requires_grad_().reshape(-1, 1)
+  labels = torch.from_numpy(y).reshape(-1, 1)
+
+  optimizer.zero_grad()
+  outputs = model(inputs)
+  loss = loss_fn(outputs, labels)
+  loss.backward()
+  optimizer.step()
+  print('epoch {}, loss {}'.format(epoch, loss.item()))
+  models.append([model.weight.item(), model.bias.item()])
+
+
+cm = pylab.get_cmap('terrain')
+fig, ax = plt.subplots()
+plt.xlabel('Bias')
+plt.ylabel('Weight')
+i = ax.imshow(losses, cmap=cm, interpolation='nearest', extent=[-5, 8, -5, 8])
+model_weights, model_biases = zip(*models)
+ax.scatter(model_biases, model_weights, c='r', marker='+')
+ax.plot(model_biases, model_weights, c='r')
+fig.colorbar(i)
+print('Figure 4. Visualized gradient descent down loss function.')
+plt.show()
+
+
+# ===
+
+def learn(criterion, x, y, lr=0.1, epochs=100, momentum=0, weight_decay=0, dampening=0, nesterov=False):
   model = torch.nn.Linear(1, 1)
-  # model.weight.data.fill_(-1.0)
-  # model.bias.data.fill_(-1.0)
-  # Y axis of image = WEIGHT
   model.weight.data.fill_(6.0)
-  # X axis of image = BIAS
-  model.bias.data.fill_(7.0)
+  model.bias.data.fill_(-3.0)
   models = [[model.weight.item(), model.bias.item()]]
 
-  optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+  optimizer = torch.optim.SGD(
+    model.parameters(),
+    lr=lr,
+    momentum=momentum,
+    weight_decay=weight_decay,
+    dampening=dampening,
+    nesterov=nesterov)
   for epoch in range(epochs):
     inputs = torch.from_numpy(x).requires_grad_().reshape(-1, 1)
     labels = torch.from_numpy(y).reshape(-1, 1)
 
     # Clear gradients w.r.t. parameters
     optimizer.zero_grad()
-    # outputs = model[0] + model[1] * inputs
     outputs = model(inputs)
-
-    # print(inputs, outputs)
-    # import pdb; pdb.set_trace()
-    # models.append([_.item() for _ in outputs])
-
-    # Calculate Loss
     loss = criterion(outputs, labels)
     loss.backward()
     optimizer.step()
-    # breakpoint()
 
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
+    # print('epoch {}, loss {}'.format(epoch, loss.item()))
     models.append([model.weight.item(), model.bias.item()])
 
-  return models
+  return model, models
 
 
-epochs = 100
-learning_rate = 0.1
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-for loss_fn, ax in [
-  (torch.nn.MSELoss(), ax1),
-  (torch.nn.L1Loss(), ax2),
-  (torch.nn.HuberLoss(), ax3),
-  (torch.nn.SmoothL1Loss(), ax4),
-]:
-  # loss_fn = loss_fns[0]
-  losses_mse = get_loss_map(loss_fn, x, y)
-  models_mse = learn(loss_fn, x, y, lr=learning_rate, epochs=epochs)
-  # breakpoint()
+def multi_plot(lr=0.1, epochs=100, momentum=0, weight_decay=0, dampening=0, nesterov=False):
+  fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+  for loss_fn, title, ax in [
+    (torch.nn.MSELoss(), 'MSELoss', ax1),
+    (torch.nn.L1Loss(), 'L1Loss', ax2),
+    (torch.nn.HuberLoss(), 'HuberLoss', ax3),
+    (torch.nn.SmoothL1Loss(), 'SmoothL1Loss', ax4),
+  ]:
+    losses = get_loss_map(loss_fn, x, y)
+    model, models = learn(
+      loss_fn, x, y, lr=lr, epochs=epochs, momentum=momentum,
+      weight_decay=weight_decay, dampening=dampening, nesterov=nesterov)
 
-  cm = pylab.get_cmap('RdYlGn')
-  cm = pylab.get_cmap('terrain')
-  # fig, ax = plt.subplots()
-  i = ax.imshow(losses_mse, cmap=cm, interpolation='nearest')
+    cm = pylab.get_cmap('terrain')
+    i = ax.imshow(losses, cmap=cm, interpolation='nearest', extent=[-5, 8, -5, 8])
+    ax.title.set_text(title)
+    loss_w, loss_b = zip(*models)
+    ax.scatter(loss_b, loss_w, c='r', marker='+')
+    ax.plot(loss_b, loss_w, c='r')
+    fig.colorbar(i)
 
-  # breakpoint()
-  loss_correction_fnx = lambda x: (x + 5.0) * 100.0 / 13.0
-  loss_correction_fny = lambda y: (y + 5.0) * 100.0 / 13.0
-  loss_x = [loss_correction_fnx(_[1]) for _ in models_mse]
-  loss_y = [loss_correction_fny(_[0]) for _ in models_mse]
+  plt.show()
 
-  ax.scatter(loss_x, loss_y, c='r')
-  ax.plot(loss_x, loss_y, c='r')
+print('Figure 5. Visualized gradient descent down all loss functions.')
+multi_plot(lr=0.1, epochs=100)
 
-  # test_x = [-4.0, -4.0, 7.0]
-  # test_y = [-4.0, 1.5, 7.0]
-  # ax.scatter(list(map(loss_correction_fnx, test_x)), list(map(loss_correction_fny, test_y)))
-  # ax.plot(test_y, test_x)
+print('Figure 6. Visualized gradient descent down all loss functions with high momentum.')
+multi_plot(lr=0.1, epochs=100, momentum=0.9)
 
-  print(loss_fn)
-  print(models_mse[0])
-  print(models_mse[-1])
-  # fig.colorbar(i)
+# N.B. Figure 7 not generated by Python.
 
-plt.show()
+print('Figure 8. Visualized gradient descent down all loss functions with high Nesterov momentum.')
+multi_plot(lr=0.1, epochs=100, momentum=0.9, nesterov=True)
+
+print('Figure 9. Visualized gradient descent down all loss functions with high Nesterov momentum and weight decay.')
+multi_plot(lr=0.1, epochs=100, momentum=0.9, nesterov=True, weight_decay=2.0)
+
+print('Figure 10. Visualized gradient descent down all loss functions with high momentum and high dampening.')
+multi_plot(lr=0.1, epochs=100, momentum=0.9, dampening=0.8)
 
